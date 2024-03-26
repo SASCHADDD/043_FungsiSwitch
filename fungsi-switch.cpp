7c6b2a67251a4da6bdda764f51515890d8c75de1
SASCHADDD <saschadanu23@gmail.com>
2024-03-26 17:56:40 +0700

    pilihan

--- fungsi-switch.cpp
+++ fungsi-switch.cpp
@@ -13,10 +13,19 @@ void input (bilA , bilB);
 
 int penjumlahan (int a, int b)
 {
-return a + b;
+    return a + b;
 }
 int pengurangan (int a , int b)
 {
-return a - b;
+    return a - b;
+}
+int perkalian (int a , int b)
+{
+    return a * b:
+}
+float pembagian (float a , float b)
+{
+    return a / b;
 }
 
+int pilihan;
