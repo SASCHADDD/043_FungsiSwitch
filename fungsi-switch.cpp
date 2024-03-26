96c6fd3e0efb458c8ddb11c681dfce87e61ebd71
SASCHADDD <saschadanu23@gmail.com>
2024-03-26 18:21:43 +0700

    switch

--- fungsi-switch.cpp
+++ fungsi-switch.cpp
@@ -29,3 +29,27 @@ float pembagian (float a , float b)
 }
 
 int pilihan;
+do
+{
+    cout << "Kalkulator sederhana"<< endl;
+    cout << "=====================" << endl;
+    cout << "1. penjumlahan" << endl:
+    cout << "2. pengurangan" << endl;
+    cout << "3. perkalian" << endl:
+    cout << "4. pembagian" << endl;
+    cout << "5. keluar" << endl;
+    cout << "3. ==================" << endl:
+    cout << "Masukkan menu pilihan : "
+    cin >> pilihan;
+
+    switch (pilihan)
+    {
+    case 1:
+        input()
+        break;
+    
+    default:
+        break;
+    }
+
+} while (/* condition */);
