8b624a7e2f578bb07b6137980bcc7d76e46604f6
SASCHADDD <saschadanu23@gmail.com>
2024-03-26 17:47:17 +0700

    input

---
+++ fungsi-switch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilA; bilB;
+
+void input (bilA , bilB);
+{
+    cout << "Masukkan nilangan pertama = ";
+    cin >> bilA;
+    cout << "Masukkan bilangan kedua = ";
+    cin >> bilB;
+}
