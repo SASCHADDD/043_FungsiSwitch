6f08efb35cd799519f5569784674bb94bd819a37
SASCHADDD <saschadanu23@gmail.com>
2024-03-26 18:39:00 +0700

    fungsi

--- fungsi-switch.cpp
+++ fungsi-switch.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int bilA; bilB;
+int bilA; int bilB;
 
-void input (bilA , bilB);
+void input ()
 {
-    cout << "Masukkan nilangan pertama = ";
+    cout << "Masukkan bilangan pertama = ";
     cin >> bilA;
     cout << "Masukkan bilangan kedua = ";
     cin >> bilB;
@@ -15,31 +15,34 @@ int penjumlahan (int a, int b)
 {
     return a + b;
 }
+
+
 int pengurangan (int a , int b)
 {
     return a - b;
 }
+
 int perkalian (int a , int b)
 {
-    return a * b:
+    return a * b;
 }
-float pembagian (float a , float b)
+int pembagian (float a , int b)
 {
     return a / b;
 }
-
+int main(){
 int pilihan;
 do
 {
     cout << "Kalkulator sederhana"<< endl;
     cout << "=====================" << endl;
-    cout << "1. penjumlahan" << endl:
+    cout << "1. penjumlahan" << endl;
     cout << "2. pengurangan" << endl;
-    cout << "3. perkalian" << endl:
+    cout << "3. perkalian" << endl;
     cout << "4. pembagian" << endl;
     cout << "5. keluar" << endl;
-    cout << "3. ==================" << endl:
-    cout << "Masukkan menu pilihan : "
+    cout << "3. ==================" << endl;
+    cout << "Masukkan menu pilihan : ";
     cin >> pilihan;
 
     switch (pilihan)
@@ -48,9 +51,28 @@ do
         input();
         cout << "Hasil penjumlahan = " << penjumlahan (bilA,bilB) << endl;
         break;
+    case 2:
+        input();
+        cout << "Hasil pengurangan = " << pengurangan (bilA,bilB) << endl;
+        break;
+    case 3:
+        input();
+        cout << "Hasil perkalian = " << perkalian (bilA,bilB) << endl;
+        break;
+    case 4:
+        input();
+        cout <<"Hasil pembagian = " << pembagian (bilA,bilB) << endl;
+        break;
+    case 5:
+        break;
     
     default:
+        cout << "Pilihan Anda salah" << endl;
         break;
     }
 
-} while (/* condition */);
+    system("pause");
+    system("CLS");
+
+} while (pilihan !=5);
+}
